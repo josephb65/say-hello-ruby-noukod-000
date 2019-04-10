9851b0bff8dd01d08664e3835db3ad2fdd54f2b8
Billy Joseph <ingbillyjoseph@gmail.com>
2019-04-10 14:02:53 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,13 @@
 # Build your say_hello method here
+<<<<<<< HEAD
 def say_hello(name = "Ruby Programmer")
   puts "Hello #{name}!"
 end
 say_hello
+=======
+def say_hello(name = "Billy Joseph")
+  puts "Hello, #{name}"
+end 
+say_hello
+say_hello("Ruby Programmer")
+>>>>>>> 411bf8bad6aa8e1071259c1babe54323c7ce931e
